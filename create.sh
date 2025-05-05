0063466fa82cbc1972126b2229ad4d269239cea5
Miguel Martins <miguel.martins@tui.com>
2025-05-05 17:21:40 +0100

    something more

--- create.sh
+++ create.sh
@@ -1,8 +1,15 @@
+aws cloudformation create-stack --stack-name $1 \
+    --template-body file://$2   \
+    --parameters file://$3  \
+    --capabilities "CAPABILITY_NAMED_IAM"  \
+    --region=us-east-1
+
+
 #!/bin/bash
 # Script to create a CloudFormation stack
 
-STACK_NAME=$1
-TEMPLATE_FILE=$2
-PARAMETERS_FILE=$3
+# STACK_NAME=$1
+# TEMPLATE_FILE=$2
+# PARAMETERS_FILE=$3
 
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://$TEMPLATE_FILE --parameters file://$PARAMETERS_FILE --region us-east-1
+# aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://$TEMPLATE_FILE --parameters file://$PARAMETERS_FILE --region us-east-1
